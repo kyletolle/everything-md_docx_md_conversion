09392e18640f6b65531345863cb7b732b0dd38fe
Kyle Tolle <kyle@nullsix.com>
2020-01-19 17:50:14 -0700

    Extract pandoc, sed, and ways of doing things to classes

--- convert_from_docx_to_md.rb
+++ convert_from_docx_to_md.rb
@@ -6,64 +6,97 @@ puts "O: #{output_md_file}"
 (puts("Input file name must be given as an arg!") and exit) if input_docx_filename.empty?
 (puts("Output file name must be given as an arg!") and exit) if output_md_file.empty?
 
-def build_pandoc_command(input_docx_filename, output_md_file)
-  <<CMD
+_original_command = <<HTML
+pandoc --filter ./markdown_underscore_italics.rb  -s --wrap=none -t gfm -o ch2/index.with-pandoc.md ch2/ch2.with-pandoc.docx && sed -i '' -e 's/ToUnDeRsCoRe/_/g' -e s/\’/\'/g -e s/\“/\"/g -e s/\”/\"/g -e s/…/.../g -e 's/\\\!/\!/g' -e 's/\> \\---/---/' -e '$a\' ch2/index.with-pandoc.md
+HTML
+
+class PandocCommand
+  attr_accessor :input_docx_filename, :output_md_file
+
+  def initialize(input_docx_filename, output_md_file)
+    self.input_docx_filename = input_docx_filename
+    self.output_md_file = output_md_file
+  end
+
+  def command_text
+    @command_text ||= <<CMD
 pandoc --filter ./markdown_underscore_italics.rb -s --wrap=none -t gfm -o #{output_md_file} #{input_docx_filename}
 CMD
+  end
+
+  def execute
+  puts "Pandoc command:"
+  puts command_text
+  `#{command_text}`
+  end
 end
 
-# SED_USE_UNDERSCORE_FOR_ITALICS = <<SED.chomp
-# 's/ToUnDeRsCoRe/_/g'
-# SED
-# SED_REPLACE_CURLY_SINGLE_OPEN_QUOTE_WITH_STRAIGHT_SINGLE_QUOTE = <<SED.chomp
-# s/\‘/\'/g
-# SED
-# SED_REPLACE_CURLY_SINGLE_END_QUOTE_WITH_STRAIGHT_SINGLE_QUOTE = <<SED.chomp
-# s/\’/\'/g
-# SED
-# SED_REPLACE_CURLY_DOUBLE_OPEN_QUOTE_WITH_STRAIGHT_DOUBLE_QUOTE = <<SED.chomp
-# s/\“/\"/g
-# SED
-# SED_REPLACE_CURLY_DOUBLE_END_QUOTE_WITH_STRAIGHT_DOUBLE_QUOTE = <<SED.chomp
-# s/\”/\"/g
-# SED
-# SED_REPLACE_ELLIPSIS_WITH_DOTDOTDOT = <<SED.chomp
-# s/…/.../g
-# SED
-# SED_REPLACE_ESCAPED_EXCLAMATION_POINT_WITH_REGULAR_EXCLAMATION_POINT = <<SED.chomp
-# 's/\\\!/\!/g'
-# SED
-# SED_REPLACE_ESCAPED_HR_WITH_UNESCAPED_HR = <<SED.chomp
-# 's/\> \\---/---/'
-# SED
-# SED_REPLACE_ESCAPED_OPEN_BRACKETS_WITH_UNESCAPED_OPEN_BRACKETS = <<SED.chomp
-# 's/\\\[/\[/g'
-# SED
-# SED_REPLACE_ESCAPED_CLOSE_BRACKETS_WITH_UNESCAPED_CLOSE_BRACKETS = <<SED.chomp
-# 's/\\\]/\]/g'
-# SED
-# SED_ADD_AN_ENDING_NEWLINE_IF_NEEDED = <<SED.chomp
-# '$a\'
-# SED
-# Note: These sed regexes are ugly because we have to escape special characters.
-SED_USE_UNDERSCORE_FOR_ITALICS = %Q('s/ToUnDeRsCoRe/_/g')
-SED_REPLACE_CURLY_SINGLE_OPEN_QUOTE_WITH_STRAIGHT_SINGLE_QUOTE = %Q(s/\\‘/\\'/g)
-SED_REPLACE_CURLY_SINGLE_END_QUOTE_WITH_STRAIGHT_SINGLE_QUOTE = %Q(s/\\’/\\'/g)
-SED_REPLACE_CURLY_DOUBLE_OPEN_QUOTE_WITH_STRAIGHT_DOUBLE_QUOTE = %Q(s/\\“/\\"/g)
-SED_REPLACE_CURLY_DOUBLE_END_QUOTE_WITH_STRAIGHT_DOUBLE_QUOTE = %Q(s/\\”/\\"/g)
-SED_REPLACE_ELLIPSIS_WITH_DOTDOTDOT = %Q(s/…/.../g)
-SED_REPLACE_ESCAPED_EXCLAMATION_POINT_WITH_REGULAR_EXCLAMATION_POINT = %Q('s/\\\\\!/\\!/g')
-SED_REPLACE_ESCAPED_HR_WITH_UNESCAPED_HR = %Q('s/\> \\---/---/')
-SED_REPLACE_ESCAPED_OPEN_BRACKETS_WITH_UNESCAPED_OPEN_BRACKETS = %Q('s/\\\[/\[/g')
-SED_REPLACE_ESCAPED_CLOSE_BRACKETS_WITH_UNESCAPED_CLOSE_BRACKETS = %Q('s/\\\]/\]/g')
-SED_ADD_AN_ENDING_NEWLINE_IF_NEEDED = %Q('$a\\')
-# TODO: Convert en-dash to --
-# TODO: Convert em-dash to ---
+class SedCommand
+  attr_accessor :output_md_file
 
-def build_sed_command(output_md_file)
-  <<CMD
+  def initialize(output_md_file)
+    self.output_md_file = output_md_file
+  end
+
+  # SED_USE_UNDERSCORE_FOR_ITALICS = <<SED.chomp
+  # 's/ToUnDeRsCoRe/_/g'
+  # SED
+  # SED_REPLACE_CURLY_SINGLE_OPEN_QUOTE_WITH_STRAIGHT_SINGLE_QUOTE = <<SED.chomp
+  # s/\‘/\'/g
+  # SED
+  # SED_REPLACE_CURLY_SINGLE_END_QUOTE_WITH_STRAIGHT_SINGLE_QUOTE = <<SED.chomp
+  # s/\’/\'/g
+  # SED
+  # SED_REPLACE_CURLY_DOUBLE_OPEN_QUOTE_WITH_STRAIGHT_DOUBLE_QUOTE = <<SED.chomp
+  # s/\“/\"/g
+  # SED
+  # SED_REPLACE_CURLY_DOUBLE_END_QUOTE_WITH_STRAIGHT_DOUBLE_QUOTE = <<SED.chomp
+  # s/\”/\"/g
+  # SED
+  # SED_REPLACE_ELLIPSIS_WITH_DOTDOTDOT = <<SED.chomp
+  # s/…/.../g
+  # SED
+  # SED_REPLACE_ESCAPED_EXCLAMATION_POINT_WITH_REGULAR_EXCLAMATION_POINT = <<SED.chomp
+  # 's/\\\!/\!/g'
+  # SED
+  # SED_REPLACE_ESCAPED_HR_WITH_UNESCAPED_HR = <<SED.chomp
+  # 's/\> \\---/---/'
+  # SED
+  # SED_REPLACE_ESCAPED_OPEN_BRACKETS_WITH_UNESCAPED_OPEN_BRACKETS = <<SED.chomp
+  # 's/\\\[/\[/g'
+  # SED
+  # SED_REPLACE_ESCAPED_CLOSE_BRACKETS_WITH_UNESCAPED_CLOSE_BRACKETS = <<SED.chomp
+  # 's/\\\]/\]/g'
+  # SED
+  # SED_ADD_AN_ENDING_NEWLINE_IF_NEEDED = <<SED.chomp
+  # '$a\'
+  # SED
+  # Note: These sed regexes are ugly because we have to escape special characters.
+  SED_USE_UNDERSCORE_FOR_ITALICS = %Q('s/ToUnDeRsCoRe/_/g')
+  SED_REPLACE_CURLY_SINGLE_OPEN_QUOTE_WITH_STRAIGHT_SINGLE_QUOTE = %Q(s/\\‘/\\'/g)
+  SED_REPLACE_CURLY_SINGLE_END_QUOTE_WITH_STRAIGHT_SINGLE_QUOTE = %Q(s/\\’/\\'/g)
+  SED_REPLACE_CURLY_DOUBLE_OPEN_QUOTE_WITH_STRAIGHT_DOUBLE_QUOTE = %Q(s/\\“/\\"/g)
+  SED_REPLACE_CURLY_DOUBLE_END_QUOTE_WITH_STRAIGHT_DOUBLE_QUOTE = %Q(s/\\”/\\"/g)
+  SED_REPLACE_ELLIPSIS_WITH_DOTDOTDOT = %Q(s/…/.../g)
+  SED_REPLACE_ESCAPED_EXCLAMATION_POINT_WITH_REGULAR_EXCLAMATION_POINT = %Q('s/\\\\\!/\\!/g')
+  SED_REPLACE_ESCAPED_HR_WITH_UNESCAPED_HR = %Q('s/\> \\---/---/')
+  SED_REPLACE_ESCAPED_OPEN_BRACKETS_WITH_UNESCAPED_OPEN_BRACKETS = %Q('s/\\\[/\[/g')
+  SED_REPLACE_ESCAPED_CLOSE_BRACKETS_WITH_UNESCAPED_CLOSE_BRACKETS = %Q('s/\\\]/\]/g')
+  SED_ADD_AN_ENDING_NEWLINE_IF_NEEDED = %Q('$a\\')
+  # TODO: Convert en-dash to --
+  # TODO: Convert em-dash to ---
+
+  def command_text
+    @command_text ||= <<CMD
 sed -i '' -e #{SED_USE_UNDERSCORE_FOR_ITALICS} -e #{SED_REPLACE_CURLY_SINGLE_OPEN_QUOTE_WITH_STRAIGHT_SINGLE_QUOTE} -e #{SED_REPLACE_CURLY_SINGLE_END_QUOTE_WITH_STRAIGHT_SINGLE_QUOTE} -e #{SED_REPLACE_CURLY_DOUBLE_OPEN_QUOTE_WITH_STRAIGHT_DOUBLE_QUOTE} -e #{SED_REPLACE_CURLY_DOUBLE_END_QUOTE_WITH_STRAIGHT_DOUBLE_QUOTE} -e #{SED_REPLACE_ELLIPSIS_WITH_DOTDOTDOT} -e #{SED_REPLACE_ESCAPED_EXCLAMATION_POINT_WITH_REGULAR_EXCLAMATION_POINT} -e #{SED_REPLACE_ESCAPED_HR_WITH_UNESCAPED_HR} -e #{SED_ADD_AN_ENDING_NEWLINE_IF_NEEDED} -e #{SED_REPLACE_ESCAPED_OPEN_BRACKETS_WITH_UNESCAPED_OPEN_BRACKETS} -e #{SED_REPLACE_ESCAPED_CLOSE_BRACKETS_WITH_UNESCAPED_CLOSE_BRACKETS} #{output_md_file}
 CMD
+  end
+
+  def execute
+    puts "Sed command:"
+    puts command_text
+    `#{command_text}`
+  end
 end
 
 class MarkdownText
@@ -150,37 +183,57 @@ class MarkdownText
   end
 end
 
-def perform_replacements_on_md_file(output_md_file)
-  original_md_text = File.read(output_md_file)
 
-  modified_md_text = MarkdownText.new(original_md_text).modified_text
+# Run this like:
+# ```
+# FirstWayOfDoingThings.new(input_docx_filename, output_md_file).run
+# ```
+#
+class FirstWayOfDoingThings
+  attr_accessor :input_docx_filename, :output_md_file
 
-  File.open(output_md_file, 'w') do |file|
-    file.puts modified_md_text
+  def initialize(input_docx_filename, output_md_file)
+    self.input_docx_filename = input_docx_filename
+    self.output_md_file = output_md_file
+  end
+
+  def run
+    PandocCommand.new(input_docx_filename, output_md_file).execute
+    SedCommand.new(output_md_file).execute
   end
 end
 
-_original_command = <<HTML
-pandoc --filter ./markdown_underscore_italics.rb  -s --wrap=none -t gfm -o ch2/index.with-pandoc.md ch2/ch2.with-pandoc.docx && sed -i '' -e 's/ToUnDeRsCoRe/_/g' -e s/\’/\'/g -e s/\“/\"/g -e s/\”/\"/g -e s/…/.../g -e 's/\\\!/\!/g' -e 's/\> \\---/---/' -e '$a\' ch2/index.with-pandoc.md
-HTML
+# Run this like:
+# ```
+# SecondWayOfDoingThings.new(input_docx_filename, output_md_file).run
+# ```
+#
+class SecondWayOfDoingThings
+  attr_accessor :input_docx_filename, :output_md_file
 
-def execute_command_for(input_docx_filename, output_md_file)
-  pandoc_cmd = build_pandoc_command(input_docx_filename, output_md_file)
-  # sed_cmd = build_sed_command(output_md_file)
+  def initialize(input_docx_filename, output_md_file)
+    self.input_docx_filename = input_docx_filename
+    self.output_md_file = output_md_file
+  end
 
-  # Intial way of doing things...
-  # puts "Pandoc command:"
-  # puts pandoc_cmd
-  # `#{pandoc_cmd}`
-  # puts "Sed command:"
-  # puts sed_cmd
-  # `#{sed_cmd}`
+  def run
+    PandocCommand.new(input_docx_filename, output_md_file).execute
 
-  # Second way of doing things...
-  puts "Pandoc command:"
-  puts pandoc_cmd
-  `#{pandoc_cmd}`
-  perform_replacements_on_md_file(output_md_file)
+    puts "Modifying text..."
+    perform_replacements_on_md_file
+  end
+
+private
+
+  def perform_replacements_on_md_file
+    original_md_text = File.read(output_md_file)
+
+    modified_md_text = MarkdownText.new(original_md_text).modified_text
+
+    File.open(output_md_file, 'w') do |file|
+      file.puts modified_md_text
+    end
+  end
 end
 
-execute_command_for(input_docx_filename, output_md_file)
+SecondWayOfDoingThings.new(input_docx_filename, output_md_file).run
