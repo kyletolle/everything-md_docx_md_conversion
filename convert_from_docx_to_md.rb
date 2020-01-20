52d9c2ed3c4201c6252c24584018056194ae19d3
Kyle Tolle <kyle@nullsix.com>
2020-01-19 17:31:11 -0700

    Add way to modify code directly in Ruby
    Instead of farming the work out to sed, we'll do it in Ruby! This makes things a lot easier to understand.

--- convert_from_docx_to_md.rb
+++ convert_from_docx_to_md.rb
@@ -66,21 +66,121 @@ sed -i '' -e #{SED_USE_UNDERSCORE_FOR_ITALICS} -e #{SED_REPLACE_CURLY_SINGLE_OPE
 CMD
 end
 
+class MarkdownText
+  attr_accessor :initial_text
+
+  def initialize(initial_text)
+    self.initial_text = initial_text
+  end
+
+  def modified_text
+    modified_text = initial_text
+
+    modification_methods.each do |modification_method|
+      modified_text = self.send(modification_method, modified_text)
+    end
+
+    modified_text
+  end
+
+  private
+
+  def modification_methods
+    # TODO:
+    #   - Use underscore in italics
+    #   - Replace curly single open quote with straight single quote
+    #   - Replace curly single end quote with straight single quote
+    #   - Replace curly double open quote with straight double quote
+    #   - Replace curly double end quote with straight double quote
+    #   - Replace ellipsis with ...
+    #   - Replace escaped ! with unescaped !
+    #   - Replace escaped HR with unescaped HR
+    #   - Replace escaped open square bracket with unescaped open square bracket
+    #   - Replace escaped close square bracket with unescaaped close square
+    #   bracket
+    #   - Add ending newline if needed
+    #   - Maybe: Convert en-dash to --
+    #   - Maybe: Convert em-dash to ---
+    %i[
+      replace_underscore_text_with_underscore_character_for_italics
+      replace_curly_single_quotes_with_straight
+      replace_curly_double_quotes_with_straight
+      replace_ellipsis_with_dotdotdot
+      replace_escaped_exclamation_point_with_unescaped
+      replace_escaped_open_square_brackets_with_unescaped
+      replace_escaped_close_square_brackets_with_unescaped
+      add_ending_newline_if_needed
+    ]
+  end
+
+  def replace_underscore_text_with_underscore_character_for_italics(text)
+    text.gsub(/ToUnDeRsCoRe/, '_')
+  end
+
+  def replace_curly_single_quotes_with_straight(text)
+    text.gsub(/[‘’]/, "'")
+  end
+
+  def replace_curly_double_quotes_with_straight(text)
+    text.gsub(/[“”]/, '"')
+  end
+
+  def replace_ellipsis_with_dotdotdot(text)
+    text.gsub(/…/, '...')
+  end
+
+  def replace_escaped_exclamation_point_with_unescaped(text)
+    text.gsub(/\\\!/, '!')
+  end
+
+  def replace_escaped_open_square_brackets_with_unescaped(text)
+    text.gsub(/\\\[/, '[')
+  end
+
+  def replace_escaped_close_square_brackets_with_unescaped(text)
+    text.gsub(/\\\]/, ']')
+  end
+
+  def add_ending_newline_if_needed(text)
+    if text.end_with?("\n")
+      text
+    else
+      "#{text}\n"
+    end
+  end
+end
+
+def perform_replacements_on_md_file(output_md_file)
+  original_md_text = File.read(output_md_file)
+
+  modified_md_text = MarkdownText.new(original_md_text).modified_text
+
+  File.open(output_md_file, 'w') do |file|
+    file.puts modified_md_text
+  end
+end
+
 _original_command = <<HTML
 pandoc --filter ./markdown_underscore_italics.rb  -s --wrap=none -t gfm -o ch2/index.with-pandoc.md ch2/ch2.with-pandoc.docx && sed -i '' -e 's/ToUnDeRsCoRe/_/g' -e s/\’/\'/g -e s/\“/\"/g -e s/\”/\"/g -e s/…/.../g -e 's/\\\!/\!/g' -e 's/\> \\---/---/' -e '$a\' ch2/index.with-pandoc.md
 HTML
 
 def execute_command_for(input_docx_filename, output_md_file)
   pandoc_cmd = build_pandoc_command(input_docx_filename, output_md_file)
-  sed_cmd = build_sed_command(output_md_file)
+  # sed_cmd = build_sed_command(output_md_file)
 
+  # Intial way of doing things...
+  # puts "Pandoc command:"
+  # puts pandoc_cmd
+  # `#{pandoc_cmd}`
+  # puts "Sed command:"
+  # puts sed_cmd
+  # `#{sed_cmd}`
+
+  # Second way of doing things...
   puts "Pandoc command:"
   puts pandoc_cmd
   `#{pandoc_cmd}`
-  puts "Sed command:"
-  puts sed_cmd
-  `#{sed_cmd}`
-
+  perform_replacements_on_md_file(output_md_file)
 end
 
 execute_command_for(input_docx_filename, output_md_file)
